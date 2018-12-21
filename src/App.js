f55b9d15d9fae6430ef9e54d774db5ecb90d1eff
Szymon Wojnar <wojnar48@gmail.com>
2018-12-20 21:04:22 -0500

    Add additional styling to the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Paho from 'paho-mqtt';
 import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = (theme) => {
+  console.log(theme);
+  return {
+    layout: {
+      marginTop: theme.spacing.unit * 2,
+    },
+    appBar: {
+      position: 'relative',
+    },
+    toolbarTitle: {
+      flex: 1,
+    },
+    cardHeader: {
+      backgroundColor: theme.palette.grey[200],
+    }
+  }
+};
 
 class App extends Component {
   // TODO(SW): Move the client config into a separate file
@@ -57,41 +79,68 @@ class App extends Component {
   }
 
   render() {
+    const { classes } = this.props;
+    const { thermometer, barometer, hygrometer } = this.state;
+
     return (
-      <main className="App">
+      <React.Fragment>
         <CssBaseline />
-        <Grid container justify='center' spacing={40}>
-          <Grid item sm={6} md={4} lg={3}>
-            <Card>
-              <CardContent>
-                <Typography gutterBottom variant='h5' component='h2'>
-                  Temperature
-                </Typography>
-              </CardContent>
-            </Card>
+        <AppBar position="static" color="default" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+              BME Dashboard
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main className={classes.layout}>
+          <Grid container justify='center' spacing={40}>
+            <Grid item sm={6} md={4} lg={2}>
+              <Card>
+                <CardHeader
+                  className={classes.cardHeader}
+                  titleTypographyProps={{ align: 'center' }}
+                  title='Thermometer'
+                  />
+                <CardContent>
+                  <Typography gutterBottom variant='h5' component='h2' align='center'>
+                    {`${thermometer} Fahr`}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item sm={6} md={4} lg={2}>
+              <Card>
+                <CardHeader
+                  className={classes.cardHeader}
+                  titleTypographyProps={{ align: 'center' }}
+                  title='Barometer'
+                  />
+                <CardContent>
+                  <Typography gutterBottom variant='h5' component='h2' align='center'>
+                    {`${barometer} kPa`}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item sm={6} md={4} lg={2}>
+              <Card>
+                <CardHeader
+                  className={classes.cardHeader}
+                  titleTypographyProps={{ align: 'center' }}
+                  title='Hygrometer'
+                  />
+                <CardContent>
+                  <Typography gutterBottom variant='h5' component='h2' align='center'>
+                    {`${hygrometer} %`}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
           </Grid>
-          <Grid item sm={6} md={4} lg={3}>
-            <Card>
-              <CardContent>
-                <Typography gutterBottom variant='h5' component='h2'>
-                  Pressure
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item sm={6} md={4} lg={3}>
-            <Card>
-              <CardContent>
-                <Typography gutterBottom variant='h5' component='h2'>
-                  Humidity
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
-      </main>
+        </main>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
